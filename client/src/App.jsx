624acdeea721ef858a9e6fac8c2ba110243a9210
Muhammed Zain <muhammedzain2256@gmail.com>
2023-12-01 12:03:39 +0500

    Navbar Changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
+import Navbar from './components/Navbar'
 
 const App = () => {
   return (
     <>
-      <nav>Site Logo</nav>
+      <Navbar/>
       <Outlet/>
     </>
   )
--- client/src/home/Home.jsx
+++ client/src/home/Home.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className=''>
+      <div className='h-screen'>
+        Home
+      </div>
+      <div className='h-screen bg-gray-100'></div>
+    </div>
   )
 }
 
