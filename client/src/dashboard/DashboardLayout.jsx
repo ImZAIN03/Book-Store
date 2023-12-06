98e8d0b3a9af077e1e4421da2680c41548a09653
Muhammed Zain <muhammedzain2256@gmail.com>
2023-12-06 21:55:38 +0500

    Manage Books

--- client/src/dashboard/DashboardLayout.jsx
+++ client/src/dashboard/DashboardLayout.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
-import SideBar from './SideBar.jsx'
+import BooksNavbar from './BooksNavbar.jsx'
 
 const DashboardLayout = () => {
   return (
     <div className='flex gap-4 flex-col md:flex-row'>
-      <SideBar/>
+      <BooksNavbar/>
       <Outlet/>
     </div>
   )
